3ed6728a327f8be541aa91b2d09c47a9b6f978e0
itsd-Ali <itsdali@gmail.com>
2025-07-23 14:50:55 +0200

    Login-Funktion für Backend implementieren (Benutzername & Passwort) und mit Angular neu verbinden

---
+++ Schreibtisch/Neuer und Alter back end/books-back-end/store-books/src/main/java/com/books/store_books/controller/BookController.java
@@ -0,0 +1,46 @@
+package com.books.store_books.controller;
+
+import com.books.store_books.bookcatalog.Book;
+import com.books.store_books.bookcatalog.BookRepository;
+
+import com.books.store_books.login.Login;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:36315")
+@RestController
+@RequestMapping("/api/books")
+//  @CrossOrigin(origins = "*") // للسماح للفرونت اند بالوصول
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Autowired
+    private BookRepository repository;
+
+    // الحصول على جميع الكتب
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+    // البحث بالعنوان
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestParam("query") String query) {
+        return bookRepository.findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(query, query);
+    }
+
+
+    // إضافة كتاب جديد
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return repository.save(book);
+    }
+}
