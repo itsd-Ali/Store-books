ed7931db5159fe8ec587bd8bb04e0d5085c0e6e8
itsd-Ali <itsdali@gmail.com>
2025-07-28 10:41:14 +0200

    version 1.0.2 Added support for ISBN field (editable, fetched from backend). Automatically generate imageUrl based on user input (number → assets/images/{number}.jpg). Preserved existing UI design.

---
+++ Schreibtisch/My-Project-Itsd/backend/books-back-end/store-books/src/main/java/com/books/store_books/StoreBooksApplication.java
@@ -0,0 +1,12 @@
+package com.books.store_books;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreBooksApplication.class, args);
+	}
+
+}
---
+++ Schreibtisch/My-Project-Itsd/backend/books-back-end/store-books/src/main/java/com/books/store_books/user/AppUser.java
@@ -0,0 +1,50 @@
+package com.books.store_books.user;
+
+import jakarta.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role = "USER"; // ROLE_USER, ROLE_ADMIN
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
